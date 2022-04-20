d9dd732ac5590e3a95a33d3e8cf36ce6d0e8ef09
Rick0317 <201507037f@edu.nishiyamato.ed.jp>
2022-04-19 22:31:57 -0400

    removing orders

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_03_26_232305) do
+ActiveRecord::Schema.define(version: 2022_03_26_193503) do
 
   create_table "active_storage_attachments", charset: "utf8", force: :cascade do |t|
     t.string "name", null: false
@@ -48,12 +48,6 @@ ActiveRecord::Schema.define(version: 2022_03_26_232305) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["user_id"], name: "index_books_on_user_id"
-  end
-
-  create_table "orders", charset: "utf8", force: :cascade do |t|
-    t.integer "price", null: false
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
   end
 
   create_table "users", charset: "utf8", force: :cascade do |t|
